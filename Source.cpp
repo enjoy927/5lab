710b6789f2ecc283192a059bfc1484bb33f0103e
enjoy927 <61457866+enjoy927@users.noreply.github.com>
2020-11-11 18:58:18 +0200

    1 завдання

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "cData.cpp"
+
+int main() {
+	CDataSimple ds;
+	ds.Generate(16, 20);
+	return 0;
+}
---
+++ cData.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+class CDataChain {
+public:
+	//CDataChain(void);
+	//virtual ~CDataChain(void);
+	//віртуальна функція, що має генерувати повну послідовність цифр.
+	virtual void Generate(int nDepth, int nMaxLength = 0) = 0;
+	//повертає довжину згенерованої послідовності цифр.
+	//size_t GetLength();
+	//повертає позицію входження підстрічки sSubStr починаючи із nPos.
+	// -1, якщо підстрічки не знайдено
+	//int Find(const char *sSubStr, int nPos = 0);
+	//повертає підстрічку починаючи із nPos довжиною nLength. або до кінця послідовності
+	//std::string GetSubStr(int nPos, int nLength = -1);
+	//повертає всю згенеровану послідовність цифр
+	//const char *GetFullString();
+protected:
+	//стрічка із згенерованою послідовністю цифр
+	std::string m_sChain;
+};
+
+class CDataSimple: public CDataChain {
+public:
+	//CDataSimple(void) {}
+	//~CDataSimple(void) override {}
+	//Реалізація генератора повної послідовності цифр
+	void Generate(int nDepth, int nMaxLength = 0) override {
+		int i = 0;
+		while (size(m_sChain) < nMaxLength) {
+			if (i > nDepth)
+				break;
+
+			m_sChain += std::to_string(i);
+			i++;
+		}
+	}
+};
