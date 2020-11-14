a68e0f0123772f90cd872a48b0d4c643bc5825af
enjoy927 <61457866+enjoy927@users.noreply.github.com>
2020-11-14 19:41:36 +0200

    added run()

--- Dialog.cpp
+++ Dialog.cpp
@@ -5,13 +5,17 @@
 typedef void (*ptDlgCommandFunc)(CWorkspace &ws);
 class CDlgCommand {
 public:
-	//CDlgCommand(const char *sName, ptDlgCommandFunc pFunc = NULL);
+	CDlgCommand(const char *sName, ptDlgCommandFunc pFunc = NULL) {
+		m_sName = sName;
+		m_pFunc = pFunc;
+	}
 	//~CDlgCommand(void);
 
 	//Отримання назви команди
 	const char *GetName() { return m_sName.c_str(); }
 	//Виклик зв"язаної функції
-	void Run(CWorkspace &ws);
+	void Run(CWorkspace &ws) {		m_pFunc(ws);
+	}
 private:
 	//Назва команди
 	std::string m_sName;
@@ -19,12 +23,28 @@ private:
 	ptDlgCommandFunc m_pFunc;
 };class CDialogManager {
 public:
-	CDialogManager(CWorkspace &ws): m_refWorkspace(ws) {}
+	CDialogManager(CWorkspace &ws): m_refWorkspace(ws) {
+		m_aCommands.push_back(new CDlgCommand("Quit"));
+	}
 	//~CDialogManager(void);
 	//Реєструє нову команду з ім"ям sName і функцією pFunc
-	void RegisterCommand(const char *sName, ptDlgCommandFunc pFunc);
+	void RegisterCommand(const char *sName, ptDlgCommandFunc pFunc) {
+		m_aCommands.push_back(new CDlgCommand(sName, pFunc));
+	}
 	//Виконує цикл обробки команд користувача
-	void Run();
+	void Run() {
+		int nCommand = 1;
+
+		while (nCommand) {
+			std::cout << std::endl;
+			for (size_t i = 0; i < m_aCommands.size(); i++)
+				std::cout << i << ". " << m_aCommands[i]->GetName() << std::endl;
+			std::cout << "Enter command:";
+			std::cin >> nCommand;
+			if ((nCommand > 0) && (nCommand < (int)m_aCommands.size()))
+				m_aCommands[nCommand]->Run(m_refWorkspace);
+		}
+	}
 private:
 	//Вектор вказівників на зареєстровані команди
 	std::vector<CDlgCommand *> m_aCommands;
--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
-#include "cWork.cpp"
+#include "Dialog.cpp"
+
+void Save(CWorkspace &ws) {
+	std::cout << "save func";
+}
 
 int main() {
 	CDataSimple ds;
 	CWorkspace ws(ds);
-	//ds.Generate(15, 20);
-	std::string path = "saving.txt";
-	ws.Load(path);
-	std::cout << ws.GetChainString() << std::endl;
+	CDialogManager mgr(ws);
+	mgr.RegisterCommand("Save", Save);
+	mgr.Run();
 	return 0;
 }
